0c0bd227b62e438494e26e6b7e4f21785df00a40
vinzenzm <vinzenz@malke.info>
2025-02-07 12:03:20 +0100

    working info publisher

--- src/padflies/padflies/connection_manager.py
+++ src/padflies/padflies/connection_manager.py
@@ -1,18 +1,21 @@
 from rclpy.node import Node
 from rclpy.callback_groups import MutuallyExclusiveCallbackGroup
 from rclpy.publisher import Publisher
+from rclpy.timer import Timer
 from padflies_interfaces.srv import Connect
 from std_srvs.srv import Empty as EmptySrv
 from rclpy.qos import QoSProfile, ReliabilityPolicy, HistoryPolicy, DurabilityPolicy
 
 from crazyflies.crazyflie import CrazyflieType
 
-from padflies_interfaces.msg import AvailabilityInfo
+from padflies_interfaces.msg import AvailabilityInfo, PadflieInfo
 
 from .charge_controller import ChargeController
 from .commander import PadflieCommander
 from ._padflie_states import PadFlieState
 from ._qos_profiles import qos_profile_performance
+
+from typing import Optional
 
 
 class ConnectionManager:
@@ -35,6 +38,9 @@ class ConnectionManager:
 
         self.__connected = False
         self.__availability_rate: float = 1.0
+        self.__info_rate: float = 0.1
+        self.__info_timer: Optional[Timer] = None
+        self.__info_publisher: Optional[Publisher] = None
 
         self.current_priority_id: int = 0
 
@@ -64,13 +70,32 @@ class ConnectionManager:
             callback_group=callback_group,
         )
 
+    def _on_connect(self):
+        self.__connected = True
+        self.__padflie_commander.set_current_priority_id(self.current_priority_id)
+
+        self.__info_timer = self.__node.create_timer(self.__info_rate, self._send_info)
+        self.__info_publisher = self.__node.create_publisher(
+            msg_type=PadflieInfo, topic=self.__prefix + "/info", qos_profile=10
+        )
+
+    def _on_disconnect(self):
+        self.__connected = False
+        self.current_priority_id += 1
+
+        if self.__info_timer is not None:
+            self.__node.destroy_timer(self.__info_timer)
+            self.__info_timer = None
+        if self.__info_publisher is not None:
+            self.__node.destroy_timer(self.__info_publisher)
+            self.__info_publisher = None
+
     def _handle_connect_request(
         self, request: Connect.Request, response: Connect.Response
     ):
         self.__node.get_logger().info(f"Request. {self.__connected}")
         if not self.__connected:  # Check padflie state IDLE ??
-            self.__connected = True
-            self.__padflie_commander.set_current_priority_id(self.current_priority_id)
+            self._on_connect()
             response.priority_id = self.current_priority_id
             response.success = True
         else:
@@ -80,9 +105,23 @@ class ConnectionManager:
     def _handle_disconnect_request(
         self, req: EmptySrv.Request, response: EmptySrv.Response
     ):
-        self.current_priority_id += 1
-        self.__connected = False
+        self._on_disconnect()
         return response
+
+    def _send_info(self):
+        msg = PadflieInfo()
+        pose_world = self.__padflie_commander.get_cf_pose_world()
+        if pose_world is not None:
+            msg.pose_world = pose_world
+            msg.pose_world_valid = True
+
+        pose = self.__padflie_commander.get_cf_pose_stamped()
+        if pose is not None:
+            msg.pose = pose
+            msg.pose_valid = True
+
+        if self.__info_publisher is not None:
+            self.__info_publisher.publish(msg)
 
     def _send_availability_info(self):
         # Publish information about us.
