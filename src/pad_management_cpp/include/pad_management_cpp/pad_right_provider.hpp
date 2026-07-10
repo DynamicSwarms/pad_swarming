0eafd5804a2beaef91a2359603aa6042e786ddb6
vinzenzm <vinzenz@malke.info>
2026-07-10 10:46:46 +0200

    some work

--- src/pad_management_cpp/include/pad_management_cpp/pad_right_provider.hpp
+++ src/pad_management_cpp/include/pad_management_cpp/pad_right_provider.hpp
@@ -12,18 +12,7 @@
 class PadRightActionServerNode : public rclcpp::Node
 {
 public:
-  explicit PadRightActionServerNode(const rclcpp::NodeOptions & options);
-
-private:
-  std::shared_ptr<IPadResourceManager> create_pad_resource_manager();
-
-private:
-  pluginlib::ClassLoader<IPadResourceManager> m_pad_resource_manager_loader;
-  std::shared_ptr<IPadResourceManager> m_pad_resource_manager;
-  std::shared_ptr<PadRightServer> m_pad_right_server;
-};
-
-inline PadRightActionServerNode::PadRightActionServerNode(const rclcpp::NodeOptions & options)
+  explicit PadRightActionServerNode(const rclcpp::NodeOptions & options)
   : Node("megapad", options)
   , m_pad_resource_manager_loader("pad_management_cpp", "IPadResourceManager")
   , m_pad_resource_manager(create_pad_resource_manager())
@@ -39,29 +28,35 @@ inline PadRightActionServerNode::PadRightActionServerNode(const rclcpp::NodeOpti
       this->get_node_waitables_interface(),
       this->get_node_logging_interface()
     ))
-{
-  RCLCPP_INFO(this->get_logger(), "PadRightActionServerNode has been initialized.");
-}
+  {
+    RCLCPP_INFO(this->get_logger(), "PadRightActionServerNode has been initialized.");
+  }
 
-inline std::shared_ptr<IPadResourceManager>
-PadRightActionServerNode::create_pad_resource_manager()
-{
-  const auto plugin_name = this->declare_parameter<std::string>(
-    "pad_resource_manager_plugin",
-    "megapad::MegaPadResourceManager");
-  RCLCPP_INFO(this->get_logger(), "Loading PadResourceManager plugin: %s", plugin_name.c_str());
+private:
+  std::shared_ptr<IPadResourceManager> create_pad_resource_manager()
+  {
+    const auto plugin_name = this->declare_parameter<std::string>(
+      "pad_resource_manager_plugin",
+      "megapad::MegaPadResourceManager");
+    RCLCPP_INFO(this->get_logger(), "Loading PadResourceManager plugin: %s", plugin_name.c_str());
 
-  pad_management_cpp::NodeInterfacesBundle node_interfaces_bundle{
-      this->get_node_base_interface(),
-      this->get_node_topics_interface(),
-      this->get_node_services_interface(),
-      this->get_node_parameters_interface(),
-      this->get_node_timers_interface(),
-      this->get_node_clock_interface(),
-      this->get_node_logging_interface(),
-      this->get_node_graph_interface(),
-      this->get_node_waitables_interface()
-  };
+    pad_management_cpp::NodeInterfacesBundle node_interfaces_bundle{
+        this->get_node_base_interface(),
+        this->get_node_topics_interface(),
+        this->get_node_services_interface(),
+        this->get_node_parameters_interface(),
+        this->get_node_timers_interface(),
+        this->get_node_clock_interface(),
+        this->get_node_logging_interface(),
+        this->get_node_graph_interface(),
+        this->get_node_waitables_interface()
+    };
 
-  return m_pad_resource_manager_loader.createSharedInstance(plugin_name, node_interfaces_bundle);
-}
+    return m_pad_resource_manager_loader.createSharedInstance(plugin_name, node_interfaces_bundle);
+  }
+
+private:
+  pluginlib::ClassLoader<IPadResourceManager> m_pad_resource_manager_loader;
+  std::shared_ptr<IPadResourceManager> m_pad_resource_manager;
+  std::shared_ptr<PadRightServer> m_pad_right_server;
+};
--- src/pad_management_cpp/include/pad_management_cpp/request.hpp
+++ src/pad_management_cpp/include/pad_management_cpp/request.hpp
@@ -36,30 +36,157 @@ public:
         std::shared_ptr<rclcpp::node_interfaces::NodeClockInterface> node_clock_interface,
         const std::shared_ptr<PadRightControlGoalHandleT> & goal_handle,
         IPadResourceManager & pad_resource_manager,
-        std::shared_ptr<PadExecuteClient> pad_execute_client);
+        std::shared_ptr<PadExecuteClient> pad_execute_client)
+        :   m_logger(logger.get_child("[" + goal_handle->get_goal()->name + "]"))
+        ,   m_clock(node_clock_interface->get_clock())
+        ,   m_name(goal_handle->get_goal()->name)
+        ,   m_request_time(m_clock->now())
+        ,   m_max_wait_time(duration_from_seconds(goal_handle->get_goal()->max_wait_time))
+        ,   m_usage_time(duration_from_seconds(goal_handle->get_goal()->usage_time))
+        ,   m_goal_handle(goal_handle)
+        ,   m_resource_manager(pad_resource_manager)
+        ,   m_pad_execute_client(pad_execute_client)
+    {
+        try {
+            std::string id_str = m_name.substr(7);
+            m_id = std::stoi(id_str);
+        } catch (const std::exception & e) {
+            (void)e;
+            RCLCPP_WARN(m_logger, "Failed to extract ID from node name: %s", m_name.c_str());
+        }
+    }
 
-    ~Request();
+    ~Request()
+    {
+        RCLCPP_INFO(m_logger, "Destroying request object.");
+
+        if (is_finished()) {
+            auto result = std::make_shared<PadRightControlActionT::Result>();
+            result->success = true;
+            result->reason = "Completed successfully";
+            m_goal_handle->succeed(result);
+        }
+
+        if (m_executing) {
+            m_resource_manager.release(m_id, m_pad_execute_client->result());
+        }
+    }
 
     const std::string & name() const { return m_name; }
     bool is_finished() const {return m_pad_execute_client->is_finished(); }
     uint8_t result() const {return m_pad_execute_client->result(); }
-    bool owns_lock() const;
+    bool owns_lock() const
+    {
+        return m_executing;
+    }
 
-    bool update();
+    bool update()
+    {
+        RequestState previous_state = m_state;
 
-    bool try_acquire();
-    bool hold_time_exceeded(const rclcpp::Duration & max_hold_time) const;
+        if (m_state == RequestState::Waiting) {
+            RequestData data;
+            data.id = m_id;
+            data.action = m_goal_handle->get_goal()->action;
+
+            AdmissionResponse resp = m_resource_manager.admit_request(data);
+            if (resp.result == AdmissionResponse::Result::ACCEPTED) {
+                m_executing = true;
+                m_acquire_time = m_clock->now();
+                m_resource_manager.start_execution(data);
+                m_state = RequestState::Acquired;
+                RCLCPP_INFO(m_logger, "Acquired rights.");
+            } else if (resp.result == AdmissionResponse::Result::REJECTED) {
+                auto result = std::make_shared<PadRightControlActionT::Result>();
+                result->success = false;
+                result->reason = resp.message.empty() ? "Request rejected" : resp.message;
+                m_goal_handle->abort(result);
+                m_state = RequestState::Finished;
+                RCLCPP_INFO(m_logger, "Request rejected.");
+            } else if (resp.result == AdmissionResponse::Result::PENDING) {
+            }
+        } else if (m_state == RequestState::Acquired) {
+            if (m_pad_execute_client->is_finished()) {
+                m_state = RequestState::Finished;
+                RCLCPP_INFO(m_logger, "Execution finished.");
+            }
+        }
+
+        return previous_state != m_state;
+    }
+
+    bool try_acquire()
+    {
+        if (!m_executing && m_resource_manager.try_lock(m_id, m_goal_handle->get_goal()->action)) {
+            m_executing = true;
+            m_acquire_time = m_clock->now();
+            RCLCPP_INFO(m_logger, "Acquired lock.");
+            return true;
+        }
+        return false;
+    }
+
+    bool hold_time_exceeded(const rclcpp::Duration & max_hold_time) const
+    {
+        if (m_executing && ((m_clock->now() - m_acquire_time) >= max_hold_time)) {
+            auto result = std::make_shared<PadRightControlActionT::Result>();
+            result->success = false;
+            result->reason = "Hold time exceeded";
+            m_goal_handle->abort(result);
+
+            RCLCPP_INFO(m_logger, "Hold time exceeded, aborting goal.");
+            return true;
+        }
+        return false;
+    }
+
 
     void publish_feedback(
         const rclcpp::Duration & expected_wait_time, 
-        const rclcpp::Duration & max_hold_time) const;
+        const rclcpp::Duration & max_hold_time) const
+    {
+        auto feedback = std::make_shared<PadRightControlActionT::Feedback>();
+        if (owns_lock()) {
+            feedback->status = PadRightControlActionT::Feedback::STATUS_ACQUIRED_RIGHT;
+            auto time_held = m_clock->now() - m_acquire_time;
+            feedback->time_remaining = duration_to_seconds(max_hold_time - time_held);
+
+            feedback->target_pose = geometry_msgs::msg::PoseStamped();
+            m_resource_manager.get_associated_position(m_id, feedback->target_pose);
+        } else {
+            feedback->status = PadRightControlActionT::Feedback::STATUS_WAITING_FOR_RIGHT;
+            feedback->time_remaining = duration_to_seconds(expected_wait_time);
+        }
+
+        m_goal_handle->publish_feedback(feedback);
+        RCLCPP_INFO(
+            m_logger,
+            "Publishing feedback: status %i, time_remaining: %f",
+            feedback->status,
+            feedback->time_remaining);
+    }
+
 
     rclcpp::Time request_time() const { return m_request_time; };
     rclcpp::Time acquire_time() const {return m_acquire_time;};
     rclcpp::Duration usage_time() const {return m_usage_time;};
     
 
-    bool check_cancel();
+    bool check_cancel()
+    {
+        if (m_goal_handle->is_canceling()) {
+            const bool was_owner = owns_lock();
+
+            auto result = std::make_shared<PadRightControlActionT::Result>();
+            result->success = true;
+            result->reason = "Canceled by client";
+            m_goal_handle->canceled(result);
+
+            RCLCPP_INFO(m_logger, "Canceling goal, was owner? %s", was_owner ? "Yes" : "No");
+            return true;
+        }
+        return false;
+    }
 
 private:
     rclcpp::Logger m_logger;
@@ -88,149 +215,3 @@ private:
 
     bool m_executing = false;
 };
-
-inline Request::Request(
-    rclcpp::Logger logger,
-    std::shared_ptr<rclcpp::node_interfaces::NodeClockInterface> node_clock_interface,
-    const std::shared_ptr<PadRightControlGoalHandleT> & goal_handle,
-    IPadResourceManager & pad_resource_manager,
-    std::shared_ptr<PadExecuteClient> pad_execute_client)
-:   m_logger(logger.get_child("[" + goal_handle->get_goal()->name + "]")),
-    m_clock(node_clock_interface->get_clock()),
-    m_name(goal_handle->get_goal()->name),
-    m_request_time(m_clock->now()),
-    m_max_wait_time(duration_from_seconds(goal_handle->get_goal()->max_wait_time)),
-    m_usage_time(duration_from_seconds(goal_handle->get_goal()->usage_time)),
-    m_goal_handle(goal_handle),
-    m_resource_manager(pad_resource_manager),
-    m_pad_execute_client(pad_execute_client)
-{
-    try {
-        std::string id_str = m_name.substr(7);
-        m_id = std::stoi(id_str);
-    } catch (const std::exception & e) {
-        (void)e;
-        RCLCPP_WARN(m_logger, "Failed to extract ID from node name: %s", m_name.c_str());
-    }
-}
-
-inline Request::~Request()
-{
-    RCLCPP_INFO(m_logger, "Destroying request object.");
-
-    if (is_finished()) {
-        auto result = std::make_shared<PadRightControlActionT::Result>();
-        result->success = true;
-        result->reason = "Completed successfully";
-        m_goal_handle->succeed(result);
-    }
-
-    if (m_executing) {
-        m_resource_manager.release(m_id, m_pad_execute_client->result());
-    }
-}
-
-inline bool Request::update()
-{
-    RequestState previous_state = m_state;
-
-    if (m_state == RequestState::Waiting) {
-        RequestData data;
-        data.id = m_id;
-        data.action = m_goal_handle->get_goal()->action;
-
-        AdmissionResponse resp = m_resource_manager.admit_request(data);
-        if (resp.result == AdmissionResponse::Result::ACCEPTED) {
-            m_executing = true;
-            m_acquire_time = m_clock->now();
-            m_resource_manager.start_execution(data);
-            m_state = RequestState::Acquired;
-            RCLCPP_INFO(m_logger, "Acquired rights.");
-        } else if (resp.result == AdmissionResponse::Result::REJECTED) {
-            auto result = std::make_shared<PadRightControlActionT::Result>();
-            result->success = false;
-            result->reason = resp.message.empty() ? "Request rejected" : resp.message;
-            m_goal_handle->abort(result);
-            m_state = RequestState::Finished;
-            RCLCPP_INFO(m_logger, "Request rejected.");
-        }
-    } else if (m_state == RequestState::Acquired) {
-        if (m_pad_execute_client->is_finished()) {
-            m_state = RequestState::Finished;
-            RCLCPP_INFO(m_logger, "Execution finished.");
-        }
-    }
-
-    return previous_state != m_state;
-}
-
-inline bool Request::check_cancel()
-{
-    if (m_goal_handle->is_canceling()) {
-        const bool was_owner = owns_lock();
-
-        auto result = std::make_shared<PadRightControlActionT::Result>();
-        result->success = true;
-        result->reason = "Canceled by client";
-        m_goal_handle->canceled(result);
-
-        RCLCPP_INFO(m_logger, "Canceling goal, was owner? %s", was_owner ? "Yes" : "No");
-        return true;
-    }
-    return false;
-}
-
-inline bool Request::owns_lock() const
-{
-    return m_executing;
-}
-
-inline bool Request::try_acquire()
-{
-    if (!m_executing && m_resource_manager.try_lock(m_id, m_goal_handle->get_goal()->action)) {
-        m_executing = true;
-        m_acquire_time = m_clock->now();
-        RCLCPP_INFO(m_logger, "Acquired lock.");
-        return true;
-    }
-    return false;
-}
-
-inline bool Request::hold_time_exceeded(const rclcpp::Duration & max_hold_time) const
-{
-    if (m_executing && ((m_clock->now() - m_acquire_time) >= max_hold_time)) {
-        auto result = std::make_shared<PadRightControlActionT::Result>();
-        result->success = false;
-        result->reason = "Hold time exceeded";
-        m_goal_handle->abort(result);
-
-        RCLCPP_INFO(m_logger, "Hold time exceeded, aborting goal.");
-        return true;
-    }
-    return false;
-}
-
-inline void Request::publish_feedback(
-    const rclcpp::Duration & expected_wait_time,
-    const rclcpp::Duration & max_hold_time) const
-{
-    auto feedback = std::make_shared<PadRightControlActionT::Feedback>();
-    if (owns_lock()) {
-        feedback->status = PadRightControlActionT::Feedback::STATUS_ACQUIRED_RIGHT;
-        auto time_held = m_clock->now() - m_acquire_time;
-        feedback->time_remaining = duration_to_seconds(max_hold_time - time_held);
-
-        feedback->target_pose = geometry_msgs::msg::PoseStamped();
-        m_resource_manager.get_associated_position(m_id, feedback->target_pose);
-    } else {
-        feedback->status = PadRightControlActionT::Feedback::STATUS_WAITING_FOR_RIGHT;
-        feedback->time_remaining = duration_to_seconds(expected_wait_time);
-    }
-
-    m_goal_handle->publish_feedback(feedback);
-    RCLCPP_INFO(
-        m_logger,
-        "Publishing feedback: status %i, time_remaining: %f",
-        feedback->status,
-        feedback->time_remaining);
-}
